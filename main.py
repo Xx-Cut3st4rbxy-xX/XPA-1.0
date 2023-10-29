33124a530f3b845923f92144f71cf83481075a5b
aleksander sapieha <129301816+Aleksander-Sapieha@users.noreply.github.com>
2023-10-29 20:59:48 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 from neuralintents.assistants import BasicAssistant
 
-assistant = BasicAssistant('intents.json')
+done = False
+
+def bad_message():
+    print("Sorry but i cant continiue this conversation")
+
+assistant = BasicAssistant('intents.json', method_mappings={"very_bad": bad_message})
 
 assistant.fit_model(epochs=50)
 assistant.save_model()
 
-done = False
 
 while not done:
     message = input("Enter a message: ")
     if message == "STOP":
         done = True
-    elif message == "KYS" or message == "kys":
-        print("No u")
+    if bad_message:
+        print("Sorry but i cant continiue this conversation")
         done = True
     else:
         print(assistant.process_input(message))
